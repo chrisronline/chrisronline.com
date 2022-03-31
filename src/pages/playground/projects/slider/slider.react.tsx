f1e83b01280ab73281d8bdb9e7e5ed42b15e518f
chrisronline <chrisronline@gmail.com>
2022-03-31 19:57:55 -0400

    Better slider

--- src/pages/playground/projects/slider/slider.react.tsx
+++ src/pages/playground/projects/slider/slider.react.tsx
@@ -5,7 +5,9 @@ import './slider.scss';
 
 export const SliderReact = () => {
   const slider = useRef<HTMLDivElement>();
-  const bounds = useRef<DOMRect>();
+  const sliderBounds = useRef<DOMRect>();
+  const wrapperBounds = useRef<DOMRect>();
+  const wrapper = useRef<HTMLDivElement>();
   const offset = useRef(0);
   const sliding = useRef(false);
 
@@ -15,17 +17,24 @@ export const SliderReact = () => {
 
   function move(event: React.MouseEvent<HTMLDivElement>) {
     if (sliding.current) {
-      offset.current += event.movementX;
-      event.currentTarget.style.left = `${offset.current}px`;
+      offset.current = enforceBounds(offset.current + event.movementX);
+      slider.current.style.left = `${offset.current}px`;
     }
   }
 
-  function moveTo(event: React.MouseEvent<HTMLDivElement>) {
-    if (event.currentTarget.classList.contains('slider-background')) {
-      offset.current =
-        event.clientX - bounds.current.x - bounds.current.width / 2;
-      slider.current.style.left = `${offset.current}px`;
+  function enforceBounds(left: number) {
+    const largestLeft = wrapperBounds.current.width - (sliderBounds.current.width / 2);
+    if (left > largestLeft) {
+      left = largestLeft;
+    } else if (left < 0) {
+      left = 0;
     }
+    return left;
+  }
+
+  function moveTo(event: React.MouseEvent<HTMLDivElement>) {
+    offset.current = enforceBounds(event.clientX - sliderBounds.current.x - (sliderBounds.current.width / 2));
+    slider.current.style.left = `${offset.current}px`;
   }
 
   function stop() {
@@ -33,27 +42,32 @@ export const SliderReact = () => {
   }
 
   useEffect(() => {
-    bounds.current = slider.current.getBoundingClientRect();
+    sliderBounds.current = slider.current.getBoundingClientRect();
   }, [slider.current]);
 
+  useEffect(() => {
+    wrapperBounds.current = wrapper.current.getBoundingClientRect();
+  }, [wrapper.current]);
+
   return (
-    <div className="slider-component">
-      <div className="slider-left-icon">
-        <FontAwesomeIcon icon={solid('volume-low')} />
-      </div>
-      <div className="slider-wrapper">
-        <div className="slider-background" onClick={moveTo} />
-        <div
-          className="slider-element"
-          onMouseDown={start}
-          onMouseMove={move}
-          onMouseUp={stop}
-          onMouseLeave={stop}
-          ref={slider}
-        />
-      </div>
-      <div className="slider-right-icon">
-        <FontAwesomeIcon icon={solid('volume-high')} />
+    <div
+      className="slider-component-wrapper"
+      onMouseDown={start}
+      onMouseMove={move}
+      onMouseUp={stop}
+      onMouseLeave={stop}
+    >
+      <div className="slider-component">
+        <div className="slider-left-icon">
+          <FontAwesomeIcon icon={solid('volume-low')} />
+        </div>
+        <div className="slider-wrapper" onClick={moveTo} ref={wrapper}>
+          <div className="slider-background"/>
+          <div className="slider-element" ref={slider} />
+        </div>
+        <div className="slider-right-icon">
+          <FontAwesomeIcon icon={solid('volume-high')} />
+        </div>
       </div>
     </div>
   );
